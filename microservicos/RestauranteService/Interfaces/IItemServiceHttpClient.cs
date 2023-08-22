efedc397c3af2d5683059db03f2bfdf26a15fd3a
esdras.souza <toyobr@hotmail.com>
2023-08-22 15:07:44 -0300

    feat: MicroServiços e usando docker

---
+++ microservicos/RestauranteService/Interfaces/IItemServiceHttpClient.cs
@@ -0,0 +1,9 @@
+﻿using RestauranteService.Dtos;
+
+namespace RestauranteService.Interfaces
+{
+    public interface IItemServiceHttpClient
+    {
+        public void EnviaRestauranteParaItemService(RestauranteReadDto restauranteDTO);
+    }
+}
---
+++ microservicos/RestauranteService/Service/ItemServiceHttpClientService.cs
@@ -0,0 +1,26 @@
+﻿using RestauranteService.Dtos;
+using RestauranteService.Interfaces;
+using System.Text;
+using System.Text.Json;
+
+namespace RestauranteService.Service
+{
+    public class ItemServiceHttpClientService : IItemServiceHttpClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
+        public ItemServiceHttpClientService(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _configuration = configuration;
+
+        }
+        public async void EnviaRestauranteParaItemService(RestauranteReadDto restauranteDTO)
+        {
+            var conteudoHttp = new StringContent(
+                    JsonSerializer.Serialize(restauranteDTO), Encoding.UTF8, "application/json"
+                );
+            await _httpClient.PostAsync(_configuration["ItemService"], conteudoHttp);
+        }
+    }
+}
